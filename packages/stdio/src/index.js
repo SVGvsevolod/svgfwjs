ca8a18bc685851c9587d30301ede41964e95fbe6
Vsevolod Subbotkin <vsevolod.98t@mail.ru>
2025-03-04 17:00:11 +0200

    wip

--- packages/stdio/src/index.js
+++ packages/stdio/src/index.js
@@ -1,14 +1,31 @@
 'use strict'
 
-module.exports.stdin = function stdin(promptHandler) {
-    if ('object' == typeof Bun && Bun.stdin instanceof Blob)
+function stdout(data) {
+    if ('object' == typeof Bun)
+        Bun.stdout.write(data)
+    if (process instanceof require('node:events'))
+        process.stdout.write(data)
+}
+
+module.exports.stdin = function stdin(promptHandler, opts) {
+    if ('object' == typeof opts && opts.welcome)
+        stdout(opts.welcome.indexOf('\r\n') < 0 ? opts.welcome + '\r\n' : opts.welcome)
+    if ('object' == typeof Bun)
         (async function() {
             for await (var a of Bun.stdin.stream()) {
-                promptHandler(require('./Prompt')(Buffer.from(a).toString()))
+                promptHandler(require('./Prompt')(
+                    Buffer.from(a).toString(),
+                    'object' == opts && opts.bye ? opts.bye : void 0
+                ))
             }
         })()
     if (process instanceof require('node:events'))
         process.stdin.on('data', function(a) {
-            promptHandler(require('./Prompt')(a.toString()))
+            promptHandler(require('./Prompt')(
+                a.toString(),
+                'object' == opts && opts.bye ? opts.bye : void 0
+            ))
         })
 }
+
+module.exports.stdout = stdout
